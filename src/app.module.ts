61386ab8fba278bef06c6e25b353a6cd1e76e9c1
Dhia M <merzougui.dhia@gmail.com>
2022-05-17 16:27:26 +0200

    Imported login and signup modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { SignUpModule } from './signup/signup.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, SignUpModule],
   controllers: [AppController],
   providers: [AppService],
 })

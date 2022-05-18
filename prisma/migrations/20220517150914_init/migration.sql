f772850ca240312dbb80fa70a4aaf311d7666a66
Dhia M <merzougui.dhia@gmail.com>
2022-05-18 10:21:53 +0200

    configured prisma

---
+++ prisma/migrations/20220517150914_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoginModule } from './login/login.module';
 import { SignUpModule } from './signup/signup.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [LoginModule, SignUpModule],
+  imports: [LoginModule, SignUpModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
